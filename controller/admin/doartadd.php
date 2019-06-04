e63dbad7699b496eafe28711ba1610fa0713eae7
ct91541 <ct91541@163.com>
2019-06-04 23:14:42 +0800

    '写文章去空'

--- controller/admin/doartadd.php
+++ controller/admin/doartadd.php
@@ -4,7 +4,7 @@
 // var_dump($_POST);
 
 // 2.生成文件
-file_put_contents('./data/arts/'.$_POST['title'],$_POST['content']);
+file_put_contents('./data/arts/'.trim($_POST['title']),trim($_POST['content']));
 
 // 提示信息 , 跳转到所有文章列表页
 message('添加成功!', 'all_articles');
